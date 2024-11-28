d5460ca2639c606690532c4d6533551fd1f02a70
Dron <ivanovandrey277@gmail.com>
2024-11-28 17:28:06 +0300

    Настройка серевера

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module MainProject
 
 go 1.23.3
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 
-	fmt.Println("Hello World")
 	godotenv.Load(".env")
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found it the environment")
 	}
-	fmt.Println("Port:", portString)
+	routrer := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: routrer, //обработчик
+		Addr:    ":" + portString,
+	}
+	log.Printf("Server running on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
