6577397c3cbf00bfe0094413c8662dc1a4e7bb25
Dron <ivanovandrey277@gmail.com>
2024-12-04 11:38:33 +0300

    Подключили бд,настроили миграцию, протестировал post запросы на добовление новых пользователей в БД

---
+++ handler_user.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"MainProject/internal/database"
+	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+	"net/http"
+	"time"
+)
+
+// обработчик создания пользователя
+func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Name string `json:"name"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	parms := parameters{}
+	err := decoder.Decode(&parms)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON: %s", err))
+		return
+	}
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      parms.Name,
+	})
+
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Could't create user:%s", err))
+		return
+	}
+
+	respondWithJSON(w, 200, user)
+}
--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"MainProject/internal/database"
+	"database/sql"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
 )
 
+type apiConfig struct {
+	DB *database.Queries
+}
+
 func main() {
 	// Загружаем переменные окружения из .env
 	godotenv.Load(".env")
+
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found it the environment")
 	}
+	//подключение к БД
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found it the environment")
+	}
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database")
+	}
+
+	//конфигурация API
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
+	}
+
 	// Создаём маршрутизатор
 	router := chi.NewRouter()
 
@@ -31,7 +55,8 @@ func main() {
 	// // Создаём маршруты версии API
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
-	v1Router.Get("/err", handlerErr) //endpoint error
+	v1Router.Get("/err", handlerErr)                  //endpoint error
+	v1Router.Post("/users", apiCfg.handlerCreateUser) //подключение обработчика пользователя
 	// Подключаем маршруты версии API к основному маршрутизатору
 	router.Mount("/v1", v1Router)
 
@@ -41,7 +66,7 @@ func main() {
 		Addr:    ":" + portString,
 	}
 	log.Printf("Server running on port %v", portString) // Запускаем сервер
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
